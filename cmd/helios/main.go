609364643f2fba23e1d501867af8d8039e94831a
Praneeth Ravuri <prav2500@gmail.com>
2026-07-20 16:06:55 -0500

    feat: configure pre-commit local hooks, official pre-commit action, CodeQL, and Dependabot

--- cmd/helios/main.go
+++ cmd/helios/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/praneethravuri/helios/pkg/logger"
+	"golang.org/x/term"
+	"io"
 	"net"
+	"os"
 )
 
 const socketPath = "/tmp/helios.sock"
@@ -13,6 +16,7 @@ func main() {
 
 	log.Info("Connecting to heliosd...")
 
+	// connect to unix socket
 	conn, err := net.Dial("unix", socketPath)
 	if err != nil {
 		log.Fatalw("Failed to connect to daemon", "error", err)
@@ -20,16 +24,24 @@ func main() {
 
 	defer conn.Close()
 
-	log.Info("Connected! Sending message...")
+	log.Info("Connected! Switching terminal to raw mode...")
 
-	message := "Hello from helios client!\n"
-
-	_, err = conn.Write([]byte(message))
-
+	// switch standard input into raw mode. makes sure keystrokes are sent instantly and special keybinds are not caught locally
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
-		log.Errorw("Failed to write message to connection", "error", err)
-		return
+		log.Fatalw("Failed to set raw mode", "error", err)
 	}
 
-	log.Info("Message sent successfully. Exiting")
+	// restore the terminal state to normal when the program exits
+	defer func() {
+		_ = term.Restore(int(os.Stdin.Fd()), oldState)
+	}()
+
+	// pipe socket incoming bytes to our local screen
+	go func() {
+		_, _ = io.Copy(os.Stdout, conn)
+	}()
+
+	// pipe keyboard inputs to the socket connection
+	_, _ = io.Copy(conn, os.Stdin)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/praneethravuri/helios
 go 1.26.5
 
 require (
+	github.com/creack/pty v1.1.24 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.28.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/term v0.45.0 // indirect
 )
